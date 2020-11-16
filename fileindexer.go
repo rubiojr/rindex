eab9144d88f52bd48ce80af957c0936c62d1c06a
Sergio Rubio <rubiojr@frameos.org>
2020-11-16 18:04:50 +0100

    foo

--- fileindexer.go
+++ fileindexer.go
@@ -1,9 +1,6 @@
 package rindex
 
 import (
-	"mime"
-	"path/filepath"
-
 	"github.com/blugelabs/bluge"
 	"github.com/rubiojr/rapi/repository"
 	"github.com/rubiojr/rapi/restic"
@@ -13,8 +10,5 @@ import (
 type FileDocumentBuilder struct{}
 
 func (i FileDocumentBuilder) ShouldIndex(fileID string, bindex blugeindex.BlugeIndex, node *restic.Node, repo *repository.Repository) (*bluge.Document, bool) {
-	doc := bluge.NewDocument(fileID).
-		AddField(bluge.NewTextField("mime_type", mime.TypeByExtension(filepath.Ext(node.Name))).StoreValue())
-
-	return doc, true
+	return bluge.NewDocument(fileID), true
 }
