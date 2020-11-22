6c074a9293e221424ab282a420200f37b086d6fe
Sergio Rubio <rubiojr@frameos.org>
2020-11-22 16:36:35 +0100

    Add search API

---
+++ search.go
@@ -0,0 +1,34 @@
+package rindex
+
+type SearchResultVisitor = func() bool
+type FieldVisitor = func(field string, value []byte) bool
+
+func (i *Indexer) Search(query string, fVisitor FieldVisitor, srVisitor SearchResultVisitor) (uint64, error) {
+	count := uint64(0)
+	reader, err := i.IndexEngine.OpenReader()
+	if err != nil {
+		return count, err
+	}
+	defer reader.Close()
+
+	iter, err := i.IndexEngine.SearchWithReaderAndQuery(query, reader)
+	if err != nil {
+		return count, nil
+	}
+
+	match, err := iter.Next()
+	for err == nil && match != nil {
+		count++
+		if fVisitor != nil {
+			err = match.VisitStoredFields(fVisitor)
+			if err != nil {
+				return count, err
+			}
+		}
+		if srVisitor != nil && !srVisitor() {
+			break
+		}
+		match, err = iter.Next()
+	}
+	return count, nil
+}
